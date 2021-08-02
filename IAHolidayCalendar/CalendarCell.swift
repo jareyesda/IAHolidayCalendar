cd5dede12fd538a396e80c037f9bced1739b9e0c
Juan Reyes <jareyesda@gmail.com>
2021-08-02 10:55:45 -0400

    Add project files and add Calendar UI Storyboard

---
+++ IAHolidayCalendar/CalendarCell.swift
@@ -0,0 +1,15 @@
+//
+//  CalendarCell.swift
+//  IAHolidayCalendar
+//
+//  Created by Juan Reyes on 8/2/21.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    
+    @IBOutlet var dayLabel: UILabel!
+    
+    
+}
---
+++ IAHolidayCalendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAHolidayCalendar
+//
+//  Created by Juan Reyes on 8/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
